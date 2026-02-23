9f59113e497ffe02c1567bb78b71976fa25de072
aniwat-c <aniwat-c@rmutp.ac.th>
2026-02-23 20:08:11 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -164,7 +164,7 @@ export default function Home() {
           {/* Google Maps Integration */}
           <div className="h-[400px] bg-[#E5E1DA] rounded-3xl overflow-hidden shadow-inner grayscale hover:grayscale-0 transition-all duration-700">
             <iframe 
-              src="https://www.google.com/maps/place/%E0%B8%A8%E0%B8%B9%E0%B8%99%E0%B8%A2%E0%B9%8C%E0%B8%A8%E0%B8%B4%E0%B8%A5%E0%B8%9B%E0%B8%B2%E0%B8%8A%E0%B8%B5%E0%B8%9E%E0%B8%9A%E0%B8%B2%E0%B8%87%E0%B9%84%E0%B8%97%E0%B8%A3/@14.1542899,100.5172976,17z/data=!3m1!4b1!4m6!3m5!1s0x30e27b003d4ff85b:0x7352a68d6a487ccc!8m2!3d14.1542847!4d100.5198725!16s%2Fg%2F11yv8z3t2n?entry=ttu&g_ep=EgoyMDI2MDIxOC4wIKXMDSoASAFQAw%3D%3D2" 
+             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3868.690499372899!2d100.5198725!3d14.154284699999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e27b003d4ff85b%3A0x7352a68d6a487ccc!2z4Lio4Li54LiZ4Lii4LmM4Lio4Li04Lil4Lib4Liy4LiK4Li14Lie4Lia4Liy4LiH4LmE4LiX4Lij!5e0!3m2!1sth!2sth!4v1771849091961!5m2!1sth!2sth" 
               className="w-full h-full border-0" 
               allowFullScreen={true} 
               loading="lazy" 
